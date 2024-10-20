6784406518099842a1dbbcff703636d9490f8321
Dasun <techsaralk.pro@gmail.com>
2024-10-20 22:51:48 +0530

    Exceptions added

--- App/init.php
+++ App/init.php
@@ -1,7 +1,26 @@
 <?php
 namespace App;
 
+use App\Exceptions\ExceptionHandler;
+use App\Exceptions\NotFoundException;
+use App\Exceptions\ServerErrorException;
+use App\Exceptions\UnauthorizedException;
+
 InitEnv::load();
 
 
-Router::get('/test', 'TestController', 'show');
+
+
+// In App/Router.php or wherever you're managing routing
+try {
+    // Your routing logic here
+} catch (NotFoundException $e) {
+    ExceptionHandler::handle($e);
+} catch (UnauthorizedException $e) {
+    ExceptionHandler::handle($e);
+} catch (ServerErrorException $e) {
+    ExceptionHandler::handle($e);
+} catch (\Exception $e) {
+    // Generic catch for any other exceptions
+    ExceptionHandler::handle($e);
+}
