e20b97468819c18730b80cc1fa5f80dd1f9c8ab5
Dasun <techsaralk.pro@gmail.com>
2024-10-21 16:38:25 +0530

    cleanup before release

--- App/init.php
+++ App/init.php
@@ -6,12 +6,4 @@ namespace App;
 InitEnv::load();
 
 
-Router::get('/', 'TestController', 'show');
-Router::get('/test', 'TestController', 'test');
-Router::post('/testpost', 'TestController', 'testpost');
-
-// students
-Router::get('/students', 'StudentController', 'index');
-Router::post('/students/add', 'StudentController', 'store');
-Router::put('/students/update/{id}', 'StudentController', 'update');
-Router::delete('/students/delete/{id}', 'StudentController', 'delete');
+Router::get('/', 'WelcomeController', 'index');
